037a5ba8719d4e6408c448d6789e127efb49939a
camilaosternomendes <168308212+camilaosternomendes@users.noreply.github.com>
2024-11-27 16:06:39 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -29,3 +29,53 @@ CREATE TABLE messages
     INDEX (sender_id, receiver_id)
 );
 
+
+CREATE TABLE tags (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    user_id INT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TRIGGER before_tag_insert
+    BEFORE INSERT
+    ON tags
+    FOR EACH ROW
+BEGIN
+    DECLARE tag_count INT;
+
+    -- Conta o número de tags associadas ao usuário
+    SELECT COUNT(*)
+    INTO tag_count
+    FROM tags
+    WHERE user_id = NEW.user_id;
+
+    -- Verifica se o número de tags já atingiu o limite
+    IF tag_count >= 5 THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'A user cannot have more than 5 tags';
+    END IF;
+END;
+
+CREATE TABLE relationships
+(
+    followed_id INT NOT NULL,
+    follower_id INT NOT NULL,
+    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (followed_id, follower_id),
+    FOREIGN KEY (followed_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (follower_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE notifications
+(
+    id                    INT AUTO_INCREMENT PRIMARY KEY,
+    notificationable_type ENUM ('POST', 'COMMENT', 'REVIEW') NOT NULL,
+    notificationable_id   INT                                NOT NULL,
+    message               TEXT                               NOT NULL,
+    user_id               INT                                NOT NULL,
+    created_at            DATETIME DEFAULT CURRENT_TIMESTAMP,
+    deleted_at            DATETIME DEFAULT NULL,
+    INDEX (notificationable_type, notificationable_id)
+);
